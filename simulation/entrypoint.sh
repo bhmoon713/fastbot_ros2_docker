f06b5d0086d2681649d420e359419f8a1a55c6ec
bhmoon713 <bhmoon713@gmail.com>
2025-09-04 12:04:48 +0000

    nginx not working

--- simulation/entrypoint.sh
+++ simulation/entrypoint.sh
@@ -1,13 +1,13 @@
 #! /bin/bash 
-
 set -e
+set -x
 
 # Source ROS 2 else ros2 command is not recognized
-source /opt/ros/humble/setup.bash
-source /ros2_ws/install/setup.bash
+#source /opt/ros/humble/setup.bash
+#source /ros2_ws/install/setup.bash
 
-ros2 launch rosbridge_server rosbridge_websocket_launch.xml
+#ros2 launch rosbridge_server rosbridge_websocket_launch.xml
 
 echo "$(date +'[%Y-%m-%d %T]') Starting nginx server..."
-set -x
+
 nginx -g 'daemon off;'
